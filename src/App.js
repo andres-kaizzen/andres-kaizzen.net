9ee40d9cc37b0a5187d99a334588814a55025871
Andres <andres.kaizzen@gmail.com>
2024-05-17 22:52:45 +0200

    Actualizo el diseño de la pagina

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
+// App.js
 import React from 'react';
 import './App.css';
 
 function App() {
   return (
-      <div className="App">
+    <div className="App">
+      <div className="section-gradient">
         <h1>andres-kaizzen.net</h1>
         <span>En construcción.</span>
       </div>
+      <div className="section-white">
+        <p>andres.kaizzen@gmail.com</p>
+      </div>
+    </div>
   );
 }
 
