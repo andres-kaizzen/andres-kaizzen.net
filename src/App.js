aa203dc4510a58418adf66af72d5cd778488116a
Andres <andres.kaizzen@gmail.com>
2024-05-17 21:25:52 +0200

    Reincicio la pagina

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import './App.css';
-import Calculadora from './componentes/Calculadora';
-import Reloj from './componentes/Reloj';
+
 
 function App() {
   return (
-    <Router>
       <div className="App">
-        <div className='App-header'>
-          <Link to="/calculadora" className="btn-calculadora">Calculadora</Link>
-          <Link to="/reloj" className="btn-reloj">Reloj</Link>
-        </div>
-        <Routes>
-          <Route path="/calculadora" element={<Calculadora />} />
-          <Route path="/reloj" element={<Reloj />} />
-        </Routes>
+        <h1>andres-kaizzen.net</h1>
+        <span>En construcción.</span>
       </div>
-    </Router>
   );
 }
 
